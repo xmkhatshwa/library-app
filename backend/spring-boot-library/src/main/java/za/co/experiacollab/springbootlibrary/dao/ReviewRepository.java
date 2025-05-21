3e57a01dcfdf3c6f21bb9a63b4fea9ed68a3ef96
xmkhatshwa <mkhatshwa.xola@gmail.com>
2025-05-21 17:42:44 +0200

    Add findByBookId Method on Spring Dev

--- backend/spring-boot-library/src/main/java/za/co/experiacollab/springbootlibrary/dao/ReviewRepository.java
+++ backend/spring-boot-library/src/main/java/za/co/experiacollab/springbootlibrary/dao/ReviewRepository.java
@@ -1,7 +1,12 @@
 package za.co.experiacollab.springbootlibrary.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 import za.co.experiacollab.springbootlibrary.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable page);
 }
